92ab4f59259b1420f39c854de09826ddfb1c2599
jankunasjr <robertjankunasjr@gmail.com>
2024-04-15 00:54:31 +0300

    updated .gitignore updated __init__.py added sign_up feature updated requirements.txt updated sign-up.html

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,35 @@
 from flask import Flask
+from website.extensions import db  # Import db from your extensions module
+from config import SQLALCHEMY_DATABASE_URI, \
+    SQLALCHEMY_TRACK_MODIFICATIONS  # Assuming these are defined in your config.py
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'pkpjauperdaug'
+    # Load the SQLAlchemy configuration from config.py
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+    db.init_app(app)  # Initialize db with the app
+
+    with app.app_context():
+        # Import models to ensure they are registered with SQLAlchemy
+        from . import models
 
     from .views import views
     from .auth import auth
+    from .models import Users
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(UserID):
+        return Users.query.get(int(UserID))
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import  Blueprint, flash, redirect, render_template, request, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from website.models import db, Users
+from flask_login import login_user, login_required, logout_user, current_user, LoginManager
 
 auth = Blueprint('auth', __name__)
 
@@ -11,6 +15,21 @@ def login():
 def logout():
     return"<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        Username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        user = Users.query.filter_by(Email=email).first()
+
+        new_user = Users(Email=email, Username=Username, PasswordHash=password1,  RegistrationDate=datetime.utcnow(), UserRole = "Regular")
+
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        flash('Account created!', category='success')
+        return render_template("sign-up.html")
+
     return render_template("sign-up.html")
