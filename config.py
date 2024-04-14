84a3749f6caf9218189d6afde34e309b9837002b
jankunasjr <robertjankunasjr@gmail.com>
2024-04-15 01:46:21 +0300

    (SCRUM 29, check for unique users)

---
+++ config.py
@@ -0,0 +1,6 @@
+username = 'postgres'
+password = 'jank4889'
+database = 'kesas'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{username}:{password}@localhost/{database}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = '14141'
