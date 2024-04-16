1e9136c15f9fd024279d35bbc03d12d99ecd3c4e
Nerte <nerte@Nertes-MacBook-Pro.local>
2024-04-16 06:34:37 +0300

    Scrum-37 | Added average review calculation and display

--- website/inventory.py
+++ website/inventory.py
@@ -3,14 +3,20 @@ from .models import Inventory, Reviews  # Assuming you have a Book model in your
 from . import db
 from flask import request, redirect, url_for
 from datetime import datetime
+from sqlalchemy.sql import func
 
 inventory = Blueprint('inventory', __name__)
-
 
 @inventory.route('/show-inventory')
 def show_inventory():
     user_id = 1  # Hardcoded user ID
     books = Inventory.query.filter_by(OwnerID=user_id).all()  # Fetch all books from the database owned by the user
+
+    # Calculate the average rating for each book
+    for book in books:
+        avg_rating = db.session.query(func.avg(Reviews.Rating)).filter(Reviews.BookID == book.BookID).scalar()
+        book.average_rating = round(avg_rating, 2) if avg_rating else None
+
     return render_template("inventory.html", inventory=books)
 
 @inventory.route('/submit-review', methods=['POST'])
